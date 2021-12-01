b88cc9853dce04485583b4d439f68ed710591fb1
Yesat <lionel.ieri@gmail.com>
2021-12-01 08:19:58 +0100

    cleaned up

--- 2021/01/sub-ping.py
+++ 2021/01/sub-ping.py
@@ -7,10 +7,10 @@ filename = os.path.join("input.txt")
 with open(filename) as f:
     input = f.read().splitlines()
 
-print(input[0])
-
 # %%
 count = 0
 for i in range(1,len(input)):
     if input[i]>input[i-1]:
         count += 1
+
+print(count)
