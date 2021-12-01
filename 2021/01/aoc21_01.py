b58c531fa3bdec8547addb86a5ab229c909c6a2c
Yesat <lionel.ieri@gmail.com>
2021-12-01 18:37:02 +0100

    Improved rolling sum

--- 2021/01/aoc21_01.py
+++ 2021/01/aoc21_01.py
@@ -4,8 +4,12 @@ def bigger(input):
 def rolling_sum(input):
     A = [sum(x) for x in zip(input,input[1:],input[2:])]
     return bigger(A)
+def smarter_rolling_sum(input):
+    return(sum(x<y for x,y in zip(input, input[3:])))
 
 # %%
+import os
+
 if __name__=='__main__':
     filename = os.path.join("input.txt")
 
@@ -14,3 +18,6 @@ if __name__=='__main__':
 
     print(bigger(input))
     print(rolling_sum(input))
+    print(smarter_rolling_sum(input))
+
+# %%
