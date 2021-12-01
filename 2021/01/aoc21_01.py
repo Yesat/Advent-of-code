1d61dd1af7b59ab67c3b0f73cc758af37abe3982
Lionel Ieri <lionel.ieri@gmail.com>
2021-12-01 16:34:21 +0100

    Tiny cleanup

--- 2021/01/aoc21_01.py
+++ 2021/01/aoc21_01.py
@@ -1,21 +1,16 @@
 # %%
-
-import os
-
-filename = os.path.join("input.txt")
-
-with open(filename) as f:
-    input = [int(i) for i in f.readlines()]
-
-# %%
 def bigger(input):
     return sum(x<y for x, y in zip(input,input[1:]))
-# %%
 def rolling_sum(input):
     A = [sum(x) for x in zip(input,input[1:],input[2:])]
     return bigger(A)
+
 # %%
 if __name__=='__main__':
+    filename = os.path.join("input.txt")
+
+    with open(filename) as f:
+        input = [int(i) for i in f.readlines()]
+
     print(bigger(input))
     print(rolling_sum(input))
-# %%
