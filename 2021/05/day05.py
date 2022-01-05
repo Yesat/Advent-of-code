1d8471c676737a3fc16f02861d19e0c597bf4b2a
Yesat <lionel.ieri@gmail.com>
2022-01-05 18:30:37 +0100

    Small change

--- 2021/05/day05.py
+++ 2021/05/day05.py
@@ -36,6 +36,24 @@ def count_cross(lines_g):
             i+=1
     return(i)
 
+# %% 
+def _lines(coords):
+    lines_ = []
+    for a in coords:
+        x_min = min([a[0],a[2]])
+        x_max = max([a[0],a[2]])
+        y_min = min([a[1],a[3]])
+        y_max = max([a[1],a[3]])
+        if a[0] == a[2]:
+            lines_.append([(a[0],v) for v in range(y_min,y_max+1)])
+        elif a[1] == a[3]:
+            lines_.append([(v,a[1]) for v in range(x_min,x_max+1)])
+        elif a[0]<a[2]:
+            lines_.append ([v for v in zip(range(x_min,x_max+1),range(y_min,y_max+1))])
+        else:
+            lines_.append ([v for v in zip(range(x_max,x_min-1,-1),range(y_min,y_max+1))])
+    return lines_
+
 # %%
 def main():
     coords =[]
@@ -43,7 +61,8 @@ def main():
         coords.append(treat_data(line))
     coords_g=treat_coord(coords)
     lines_g=glines(coords_g)
-    print(count_cross(lines_g))
+    lines_ = _lines(coords)
+    print(count_cross(lines_g),count_cross(lines_))
     
 
 if __name__=='__main__':
